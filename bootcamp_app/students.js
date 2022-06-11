02f2a5f0cac1712ed8ea337b64ea407665ac2f9e
Sneha Mahajan <sneh.km@gmail.com>
2022-06-11 11:37:32 -0400

    Add parameterized query for students.js

--- bootcamp_app/students.js
+++ bootcamp_app/students.js
@@ -1,13 +1,6 @@
 const { Pool } = require("pg");
 
-const getData = (arguments) => {
-  const data = arguments.slice(2);
-  const cohort = data[0];
-  const limit = parseInt(data[1]);
-  return { cohort, limit };
-};
 
-const { cohort, limit } = getData(process.argv);
 
 const pool = new Pool({
   user: "vagrant",
@@ -16,20 +9,28 @@ const pool = new Pool({
   database: "bootcampx",
 });
 
-const SQLQuery = `
-SELECT students.id, students.name, cohorts.name as cohort_name
+const text = `
+SELECT students.id, students.name, email, cohorts.name as cohort_name
 FROM students
 JOIN cohorts ON cohorts.id = cohort_id
-WHERE cohorts.name LIKE '${cohort}%'
-LIMIT ${limit};
+WHERE cohorts.name LIKE $1
+LIMIT $2;
 `;
 
+const getData = (arguments) => {
+  const data = arguments.slice(2);
+  const cohort = data[0]+"%";
+  const limit = parseInt(data[1]);
+  return [cohort, limit];
+};
+const values = getData(process.argv);
+
 pool
-  .query(SQLQuery)
+  .query(text, values)
   .then((res) => {
     res.rows.forEach((row) => {
       console.log(
-        `${row.name} has id of ${row.id} and as in the ${row.cohort_name} cohort.`
+        `${row.name} has id of ${row.id} and as in the ${row.cohort_name} cohort.${row.email}`
       );
     });
   })
